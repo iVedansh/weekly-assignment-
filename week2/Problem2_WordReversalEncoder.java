c3a16d5f741f37ac417983cb90abe01c230c5f76
VEDANSH MISHRA <vedanshmishra467@gmail.com>
2026-09-21 22:54:55 +0530

    Week 2 - Problem 2 solution

---
+++ week2/Problem2_WordReversalEncoder.java
@@ -0,0 +1,18 @@
+public class Problem2_WordReversalEncoder {
+    static String reverseEachWord(String sentence) {
+        String[] words = sentence.split(" ");
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            result.append(new StringBuilder(words[i]).reverse());
+            if (i < words.length - 1) {
+                result.append(" ");
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseEachWord("hello club"));
+    }
+}
